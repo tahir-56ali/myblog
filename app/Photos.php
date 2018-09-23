bc916d0b99c94ac75f95e6c6f55cee4c91b78741
Tahir Ali <tahir.56ali@gmail.com>
2018-09-24 00:20:58 +0500

    users listing with pictures

--- app/Photos.php
+++ app/Photos.php
@@ -6,10 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photos extends Model
 {
+    protected $uploads = '/images/';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = ['file'];
+
+    public function getFileAttribute($photo)
+    {
+        return $this->uploads . $photo;
+    }
 }
