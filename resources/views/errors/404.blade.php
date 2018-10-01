02ce1e7c4f97f08b239d6b54ba8c889dcf05e645
Tahir Ali <tahirali.sds@nxvt.com>
2018-10-01 21:39:38 +0500

    posts set routes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,7 @@
-Custom 404 Page!
+@extends('layouts.app')
+
+@section('content')
+
+    <h1 class="text-center">OPPS no page available!</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,6 @@ Route::group(['middleware' => 'admin'], function () {
 
     Route::resource('admin/users', 'AdminUsersController');
 
+    Route::resource('admin/posts', 'AdminPostsController');
+
 });
